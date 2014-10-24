9ce22de72d2ea3df6fd545abc7ab9f7ce21ffd04
tike <timflex@gmx.de>
2014-10-24 19:14:13 +0200

    refactored code in searchSvc for a bit

--- src/js/services/SearchSvc.js
+++ src/js/services/SearchSvc.js
@@ -11,31 +11,41 @@ angular.module('bookshelf')
         url: '/api/search',  
       };
     }
-
-    // Public API here
-    return {
-      // query is meant to be an object
-      search: function (query) {
-        var defered = $q.defer();
-
-        var conf = getHttpConf();
-        conf.params = query;
-        
-        $http(conf).
+    
+    function doRequest(reqConfig){
+      var defered = $q.defer();        
+        $http(reqConfig).
         success(function(data, status, headers, config){
-          $log.debug('SeSvc: success', status, data, headers(), config);
+          $log.debug('SeSvc.doRequest: success', status, data, headers(), config);
           defered.resolve(data);
         }).
         error(function(data, status, headers){
-          $log.error('SeSvc: search error', data, status, headers());
+          $log.error('SeSvc.doRequest: search error', data, status, headers());
           switch(status){
             default:
               break;
           }
           defered.reject(data);
         });
-        $log.debug('SeSvc: search ended');
+        $log.debug('SeSvc.doRequest: search ended');
         return defered.promise;
+    }
+
+    // Public API here
+    return {
+      // query is meant to be an object
+      search: function (query) {
+        return doRequest({
+          method: 'GET',
+          url: '/api/search',
+          params: query,
+        });
+      },
+      byId: function(id){
+        return doRequest({
+          method: 'GET',
+          url: '/api/book/'.concat(id),
+        });
       },
     };
   }]);
