28a4ffea31dd19c48c7165b0ab18dad8ba114c4d
tike <timflex@gmx.de>
2015-05-31 16:40:54 +0200

    tag filtering works

--- src/js/controllers/partials/BookListCtrl.js
+++ src/js/controllers/partials/BookListCtrl.js
@@ -2,9 +2,13 @@
 
 angular.module('bookshelf')
   .controller('BookListCtrl', ['$scope', '$log', '$modal', 'SearchSvc', function ($scope, $log, $modal, SearchSvc) {
-
+    // keep num active tags to simplifiy/shortcut filtering in $scope.byTags
+    var activeTags = 0;
+    
+    // start init sequence
     $scope.orderByField = ['-year', 'pages' ];
     $scope.reverseSort = false;
+    $scope.filterCond = {};
 
     SearchSvc.search({ 'title': 'Hadoop' }).then(
       function(resp){
@@ -15,12 +19,31 @@ angular.module('bookshelf')
       }
     );
     
+    // end init sequence ...
+    
     $scope.star = function(book){
       book.status ? book.status.starred = !book.status.starred : book.status = {starred: true};
       SearchSvc.upSert(book);
     };
     
+    $scope.checkTags = function(tag){
+      tag.active = !tag.active;
+      tag.active ? activeTags++ : activeTags--;
+      $log.debug('num Tags:', tag.active, activeTags);
+    };
     
+    $scope.byTags = function(book, idx){
+      if (activeTags === 0){
+        return true;
+      }
+      
+      for (var i=0; i<book.tags.length; i++){
+        var selectedTag = $scope.tags[book.tags[i]]
+        if( selectedTag && selectedTag.active){
+          return true;
+        }
+      }
+    };
     
     $scope.addTags = function(book){
       console.log('book tags:', book.tags);
